dcc203b622c83f034b045e81302dd649539bbf10
Accipiter Chalybs <pumpkinpi64@gmail.com>
2016-03-06 01:06:28 -0800

    Fixed Probe Droid Remains not despawning - now scale down and disappear

---
+++ Assets/Scripts/Decay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decay : MonoBehaviour {
+    public float shrinkTime = 2.5f;
+    public float deleteTime = 5f;
+
+    private float time = 0;
+
+    private Vector3 fullSize = new Vector3(1, 1, 1);
+    private Vector3 zeroSize = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time > shrinkTime)
+        {
+            transform.localScale = Vector3.Lerp(fullSize, zeroSize, (time - shrinkTime) / (deleteTime - shrinkTime));
+        }
+        if (time > deleteTime)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
