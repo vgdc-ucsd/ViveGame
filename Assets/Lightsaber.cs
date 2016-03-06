abe14cd62fbf91f99aec8ae50e0f155341a6d28f
Jeffrey Johnson <jeffcjohnson@live.com>
2016-03-05 20:36:29 -0800

    Destroying drones

---
+++ Assets/Lightsaber.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightsaber : MonoBehaviour
+{
+    public Transform explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        var explosion = Instantiate(explosionPrefab);
+        explosion.position = other.transform.position;
+        Destroy(other.gameObject);
+        Destroy(other.gameObject, 1);
+    }
+}
